1b8e892e4466dce6baa77b631887a3a686206c74
DivyanshuRajwar <divyanshurajwar@gmail.com>
2024-02-21 07:36:03 +0530

    New feature

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 
 const display=document.querySelector(".display");
 let value=parseInt(display.innerText);
+const btn=document.querySelector(".reset");
 const inc=()=>{
     value+=1;
     display.innerHTML=value;
+    btn.classList.remove("left");
 }
 const dec=()=>{
     value-=1;
     display.innerHTML=value;
+    btn.classList.add("left");
 }
 const reset=()=>{
     value=0;
     display.innerText=value;
+    btn.classList.remove("left");
 }
